8497ed79179a9bfe1c59802d8e77a4f20b95dae3
khanglc <91674593+khanglc@users.noreply.github.com>
2022-06-29 23:24:58 +0700

    dong bo code

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -103,7 +103,7 @@
             <i class="fas fas fa-bookmark"></i>
             <span>Quản lý trang tĩnh</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar" >
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <a class="collapse-item" href="{{route('sologan.index')}}"> <i class="far fa-caret-square-right"></i>
                     Slogan</a>
@@ -130,7 +130,8 @@
         </div>
     </li>
     <hr class="sidebar-divider my-0">
-    <li class="nav-item {{(request()->is('tinTuc')) ? 'active' : '' }} {{(request()->is('chinhSach')) ? 'active' : '' }}">
+    <li
+        class="nav-item {{(request()->is('tinTuc')) ? 'active' : '' }} {{(request()->is('chinhSach')) ? 'active' : '' }}">
         <a class="nav-link collapsed" data-toggle="collapse" data-target="#tintuc" aria-expanded="false"
             aria-controls="tintuc">
             <i class="fas fas fa-photo-video"></i>
@@ -138,7 +139,8 @@
         </a>
         <div id="tintuc" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar" style="">
             <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item " href="{{route('tinTuc.index')}}"> <i class="far fa-caret-square-right"></i> Tin
+                <a class="collapse-item " href="{{route('tinTuc.index')}}"> <i class="far fa-caret-square-right"></i>
+                    Tin
                     tức</a>
                 <a class="collapse-item" href="{{route('chinhSach.index')}}"> <i class="far fa-caret-square-right"></i>
                     Chính sách</a>
@@ -168,21 +170,3 @@
     <i class="fas fa-angle-up"></i>
 </a>
 <!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="{{route('dangnhap')}}">Logout</a>
-            </div>
-        </div>
-    </div>
-</div>
